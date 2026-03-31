54f0f746a596cd919a4f1fb085d7b55e6017a4a7
niloy <niloypaul81@gmail.com>
2026-03-31 22:06:43 +0600

    active tab added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Digitools from "./components/Digitools"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
@@ -5,6 +6,7 @@ import NavBar from "./components/NavBar"
 import Stats from "./components/Stats"
 import Steps from "./components/Steps"
 import TransparentPricing from "./components/TransparentPricing"
+import Cart from "./components/Cart"
 
 const getDigiTools=async()=>{
 const res=await fetch('/data.json')
@@ -15,6 +17,10 @@ const digiToolPromise=getDigiTools();
 
 function App() {
 
+  const [activeTab , setActiveTab] = useState('model')
+
+  console.log(activeTab);
+
 
 
   return (
@@ -22,7 +28,39 @@ function App() {
      <NavBar></NavBar>
      <Hero></Hero>
      <Stats></Stats>
-     <Digitools digiToolPromise={digiToolPromise}></Digitools>
+
+     <div>
+            <h1 className="text-center mt-14 md:mt-20 text-[#101727] text-3xl md:text-[48px] font-bold px-4">
+    Premium Digital Tools
+  </h1>
+  <p className="text-center text-[#627382] px-4 mb-14 md:mb-20 text-sm md:text-base">
+Choose from our curated collection of premium digital products designed <br/>to boost your productivity and creativity.  </p>
+           </div>
+
+
+           <div className="tabs tabs-box my-10 justify-center max-w-90 gap-3 bg-white rounded-3xl mx-auto">
+  <input  type="radio" name="my_tabs_1" className={`tab rounded-full text-center transition-all duration-300 
+        w-full sm:w-40 py-2
+        ${
+          activeTab === "model"
+            ? "text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA]"
+            : "bg-white text-black"
+        }`} aria-label="Products"  defaultChecked onClick={()=>setActiveTab('model')} />
+  <input  type="radio" name="my_tabs_1" className={`tab rounded-full text-center transition-all duration-300 
+        w-full sm:w-40 py-2
+        ${
+          activeTab === "cart"
+            ? "text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA]"
+            : "bg-white text-black"
+        }`} aria-label="Cart (0)" onClick={()=>setActiveTab('cart')} />
+  
+</div>
+
+{ activeTab=== 'model' && <Digitools digiToolPromise={digiToolPromise}></Digitools>}
+
+
+     
+   {activeTab=== 'cart' && <Cart></Cart>}  
      <Steps></Steps>
      <TransparentPricing></TransparentPricing>
      <Footer></Footer>
